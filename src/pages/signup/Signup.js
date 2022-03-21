9157df89d76ade15763838014101a2e5613461d4
sarablom <sara.cecilia.blom@gmail.com>
2022-03-21 11:38:36 +0100

    add sign up page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useState } from 'react'
 import "./Signup.css"
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbNail, setThumbNail] = useState(null);
+
   return (
-    <div>Signup</div>
+    <form className="auth-form">
+      <h2>Sign up</h2>
+      <label>
+        <span>email: </span>
+        <input 
+          required
+          type="email"
+          onChange={(e) => {setEmail(e.target.value)}}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password: </span>
+        <input 
+          required
+          type="password"
+          onChange={(e) => {setPassword(e.target.value)}}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>display name: </span>
+        <input 
+          required
+          type="text"
+          onChange={(e) => {setDisplayName(e.target.value)}}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>profile thumbnail: </span>
+        <input 
+          required
+          type="file"
+        />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
   )
 }
 
