f39277681ca624d5525dbecff880ceb06aa9c635
sarablom <sara.cecilia.blom@gmail.com>
2022-03-21 10:28:18 +0100

    add connect to database

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firbase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFe6MJQ-N-hCk_qVczYDuhHRFf-yb6-5c",
+  authDomain: "dojo-45140.firebaseapp.com",
+  projectId: "dojo-45140",
+  storageBucket: "dojo-45140.appspot.com",
+  messagingSenderId: "969321744361",
+  appId: "1:969321744361:web:f49fe0f87a164e32a52d78",
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+//init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//time stamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
