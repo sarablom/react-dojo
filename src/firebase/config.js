df18433c3ea959d2295af0d73bb973bb12c206e6
sarablom <sara.cecilia.blom@gmail.com>
2022-03-21 12:46:47 +0100

    add signup functionality

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/storage"
 
 const firebaseConfig = {
   apiKey: "AIzaSyAFe6MJQ-N-hCk_qVczYDuhHRFf-yb6-5c",
@@ -17,8 +18,9 @@ firebase.initializeApp(firebaseConfig);
 //init services
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 //time stamp
 const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, projectStorage, timestamp };
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,52 +1,88 @@
-import { useState } from 'react'
-import "./Signup.css"
+import { useState } from "react";
+import { useSignup } from "../../hooks/useSignup";
+import "./Signup.css";
 
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [displayName, setDisplayName] = useState("");
-  const [thumbNail, setThumbNail] = useState(null);
+  const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null);
+  const { signup, isPending, error } = useSignup();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signup(email, password, displayName, thumbnail);
+  }
+
+  const handlefileChange = (e) => {
+    //reset user selection
+    setThumbnail(null);
+    let selected = e.target.files[0];
+
+    if (!selected) {
+      setThumbnailError("Please select a file");
+      return;
+    }
+    if (!selected.type.includes("image")) {
+      setThumbnailError("Selected file must be an image");
+      return;
+    }
+    if (selected.size > 150000) {
+      setThumbnailError("Image file size must be less than 150kb");
+      return;
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selected);
+  };
 
   return (
-    <form className="auth-form">
+    <form className="auth-form" onSubmit={handleSubmit}>
       <h2>Sign up</h2>
       <label>
         <span>email: </span>
-        <input 
+        <input
           required
           type="email"
-          onChange={(e) => {setEmail(e.target.value)}}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
           value={email}
         />
       </label>
       <label>
         <span>password: </span>
-        <input 
+        <input
           required
           type="password"
-          onChange={(e) => {setPassword(e.target.value)}}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
           value={password}
         />
       </label>
       <label>
         <span>display name: </span>
-        <input 
+        <input
           required
           type="text"
-          onChange={(e) => {setDisplayName(e.target.value)}}
+          onChange={(e) => {
+            setDisplayName(e.target.value);
+          }}
           value={displayName}
         />
       </label>
       <label>
         <span>profile thumbnail: </span>
-        <input 
-          required
-          type="file"
-        />
+        <input required type="file" onChange={handlefileChange} />
+        {thumbnailError && <div className="error">{thumbnailError}</div>}
       </label>
-      <button className="btn">Sign up</button>
+      {!isPending && <button className="btn">Sign up</button>}
+      {isPending && <button className="btn" disabled>Loading...</button>}
+      {error && <div className="error">{error}</div>}
     </form>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
