1ed8ceebec8875a3c1fb8c6ea79a8ed5b0b4652c
sarablom <sara.cecilia.blom@gmail.com>
2022-03-21 15:01:16 +0100

    add avatar and user when logged in

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,46 @@
 import React from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import Temple from "../assets/temple.svg";
 import "./Navbar.css";
 
 function Navbar() {
-    const { logout, isPending } = useLogout();
+  const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
-  return <div className="navbar">
+  return (
+    <div className="navbar">
       <ul>
         <li className="logo">
-            <img src={Temple} alt="Dojo logo, a temple" />
-            <span>The Dojo</span>
+          <img src={Temple} alt="Dojo logo, a temple" />
+          <span>The Dojo</span>
         </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
         <li>
-            <Link to="/login">Login</Link>
-        </li>
-        <li>
-            <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-            {!isPending && <button className="btn" onClick={logout}>Logout</button>}
-            {isPending && <button className="btn" disabled>Logging out...</button>}
+          {user && !isPending && (
+            <button className="btn" onClick={logout}>
+              Logout
+            </button>
+          )}
+          {user && isPending && (
+            <button className="btn" disabled>
+              Logging out...
+            </button>
+          )}
         </li>
       </ul>
-  </div>;
+    </div>
+  );
 }
 
 export default Navbar;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,36 +1,40 @@
-import React from 'react'
-import { NavLink } from "react-router-dom"
-import "./Sidebar.css"
-import DashboardIcon from "../assets/dashboard_icon.svg"
-import AddIcon from "../assets/add_icon.svg"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import DashboardIcon from "../assets/dashboard_icon.svg";
+import AddIcon from "../assets/add_icon.svg";
+import Avatar from "./Avatar";
+import "./Sidebar.css";
 
 function Sidebar() {
+  const { user } = useAuthContext();
   return (
     <div className="sidebar">
-        <div className="sidebar-content">
-            <div className="user">
-                <p>Hey user</p>
-            </div>
-            <nav className="links">
-                <ul>
-                    <li>
-                        {/* NavLink gives an active class */}
-                    <NavLink to="/">
-                        <img src={DashboardIcon} alt="Dashboard icon" />
-                        <span>Dashboard</span>
-                    </NavLink>
-                    </li>
-                    <li>
-                    <NavLink to="/create">
-                        <img src={AddIcon} alt="Add project icon" />
-                        <span>New project</span>
-                    </NavLink>
-                    </li>
-                </ul>
-            </nav>
+      <div className="sidebar-content">
+        <div className="user">
+          <Avatar src={user.photoURL} />
+          <p>Hey {user.displayName}</p>
         </div>
+        <nav className="links">
+          <ul>
+            <li>
+              {/* NavLink gives an active class */}
+              <NavLink to="/">
+                <img src={DashboardIcon} alt="Dashboard icon" />
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/create">
+                <img src={AddIcon} alt="Add project icon" />
+                <span>New project</span>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
